e2631343c8e6290147b09fe53d07f93b487a4b63
YefriGavarrete <yefrigd12354@gmail.com>
2025-06-23 22:29:22 -0600

    First commit

--- app/src/main/java/com/example/sumadenumeros/ResultadoActivity.java
+++ app/src/main/java/com/example/sumadenumeros/ResultadoActivity.java
@@ -10,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ResultadoActivity extends AppCompatActivity {
     
     TextView txtResultado;
-    @SuppressLint("SetTextI18n")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
